5eec0c317aefe48d931e7457745356e28080f1bb
Adam <Adam.sturesson@live.se>
2024-11-04 15:02:59 +0100

    first commit

---
+++ src/main/java/org/example/springkryptering/SpringKrypteringApplication.java
@@ -0,0 +1,13 @@
+package org.example.springkryptering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKrypteringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKrypteringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springkryptering/SpringKrypteringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springkryptering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKrypteringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
